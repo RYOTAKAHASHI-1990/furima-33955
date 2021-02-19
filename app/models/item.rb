48521c95bb51ec6c4fdf2d98c696cb47c1e0e6a6
RYOTAKAHASHI-1990 <77477778+RYOTAKAHASHI-1990@users.noreply.github.com>
2021-02-19 16:10:29 +0900

    Active Storageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,14 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one :purchase_record
-  
+  has_one_attached :image
 
+  validates :name
+  validates :description
+  validates :category_id
+  validates :condition_id
+  validates :shipping_cost_id
+  validates :prefecture_id
+  validates :shipping_day_id
+  validates :price
 end
