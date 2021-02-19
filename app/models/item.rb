d584b59c1f730732c8576341b96bbb2f0da30b8c
RYOTAKAHASHI-1990 <77477778+RYOTAKAHASHI-1990@users.noreply.github.com>
2021-02-19 23:12:11 +0900

    「商品出品機能」の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,7 +13,7 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :description
-    validates :price
+    validates :price, format: {with: /\A[0-9]+\z/}, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
   end
 
   with_options numericality: { other_than: 1 } do
