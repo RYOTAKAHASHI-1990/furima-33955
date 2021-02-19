8919cab320b406875b040ccd77f3bdc83b5ff72d
RYOTAKAHASHI-1990 <77477778+RYOTAKAHASHI-1990@users.noreply.github.com>
2021-02-19 18:34:57 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,26 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :prefecture
+  belongs_to :shipping_cost
+  belongs_to :shipping_day
 
   belongs_to :user
   has_one :purchase_record
   has_one_attached :image
 
-  validates :name
-  validates :description
-  validates :category_id
-  validates :condition_id
-  validates :shipping_cost_id
-  validates :prefecture_id
-  validates :shipping_day_id
-  validates :price
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :price
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :condition_id
+    validates :shipping_cost_id
+    validates :prefecture_id
+    validates :shipping_day_id
+  end
 end
