b531da04f39662c1efd89657f527ebccb2cf8a68
RYOTAKAHASHI-1990 <77477778+RYOTAKAHASHI-1990@users.noreply.github.com>
2021-02-22 11:22:17 +0900

    private以下にメソッドを追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
+  before_action :set_item, except: [:index, :new, :create]
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, only: [:edit, :update]
+  
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -11,15 +13,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
@@ -41,7 +40,11 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :shipping_cost_id, :prefecture_id, :shipping_day_id, :price).merge(user_id: current_user.id)
   end
 
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
   def move_to_index
-    redirect_to root_path unless current_user == Item.find(params[:id]).user
+    redirect_to root_path unless current_user == set_item.user
   end
 end
