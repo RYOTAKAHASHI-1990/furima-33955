1c237a988dbe26a6c8ca92692558cc0d074517e8
RYOTAKAHASHI-1990 <77477778+RYOTAKAHASHI-1990@users.noreply.github.com>
2021-02-23 21:14:26 +0900

    テストコードの実装

---
+++ spec/factories/order_addresses.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :order_address do
+    postal_code {'123-4567'}
+    prefecture_id {2}
+    city {'東京都'}
+    address {'1-1'}
+    building_name {'東京ハイツ'}
+    phone_number {'09012345678'}
+    user_id {1}
+    item_id {1}
+
+    association :user, :item
+  end
+end
