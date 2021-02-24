b98e5c996f8e8debffcd8bede9a83753fcbdbc6b
RYOTAKAHASHI-1990 <77477778+RYOTAKAHASHI-1990@users.noreply.github.com>
2021-02-24 21:42:09 +0900

    テストコードの修正

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -45,10 +45,25 @@ RSpec.describe OrderAddress, type: :model do
       @order_address.valid?
       expect(@order_address.errors.full_messages).to include("Phone number Input only number")
     end
-    it "tokenが空では登録できないこと" do
+    it "tokenが空では保存できないこと" do
       @order_address.token = nil
       @order_address.valid?
       expect(@order_address.errors.full_messages).to include("Token can't be blank")
+    end
+    it "user_idが空では購入できないこと" do
+      @order_address.user_id = nil
+      @order_address.valid?
+      expect(@order_address.errors.full_messages).to include("User can't be blank")
+    end
+    it "item_idが空では購入できないこと" do
+      @order_address.item_id = nil
+      @order_address.valid?
+      expect(@order_address.errors.full_messages).to include("Item can't be blank")
+    end
+    it "phone_numberは数字のみでないと登録できないこと" do
+      @order_address.phone_number = 'a0901234567'
+      @order_address.valid?
+      expect(@order_address.errors.full_messages).to include("Phone number Input only number")
     end
   end
 end
