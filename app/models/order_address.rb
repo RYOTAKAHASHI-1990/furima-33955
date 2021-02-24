61561b618234e39b97265a195f22b582f49dd185
RYOTAKAHASHI-1990 <77477778+RYOTAKAHASHI-1990@users.noreply.github.com>
2021-02-24 17:08:25 +0900

    「クレジットカード決済機能」の実装

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :item_id, :user_id, :order_id
+  attr_accessor :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :item_id, :user_id, :order_id, :token
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'Input correctly' }
@@ -10,6 +10,7 @@ class OrderAddress
     validates :user_id
     validates :item_id
     validates :prefecture_id, numericality: { other_than: 1 } 
+    validates :token
   end
 
   def save
--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -8,6 +8,7 @@ FactoryBot.define do
     phone_number {'09012345678'}
     user_id {1}
     item_id {1}
+    token {"tok_abcdefghijk00000000000000000"}
 
     association :user, :item
   end
