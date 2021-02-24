6ae9b3b4cf7e9efcc43b387108d431790202f468
RYOTAKAHASHI-1990 <77477778+RYOTAKAHASHI-1990@users.noreply.github.com>
2021-02-24 20:45:09 +0900

    ログイン/ログアウトそれぞれの状態における遷移ページの設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,15 @@
 class OrdersController < ApplicationController
-  
+  before_action :authenticate_user!, only: [:index, :create]
+  before_action :set_action, expect: [:index, :create]
+
   def index
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
+    if current_user == @item.user || @item.order.present?
+       redirect_to root_path
+    end
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
       pay_item
@@ -29,6 +32,10 @@ class OrdersController < ApplicationController
       amount: @item.price,  
       card: order_params[:token],    
       currency: 'jpy'                 
-    )
+    ) 
+  end
+
+  def set_action
+    @item = Item.find(params[:item_id])
   end
 end
